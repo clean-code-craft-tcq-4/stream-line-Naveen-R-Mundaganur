ea3da040d3a1f340d9c32f5a462ff28db9192508
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-09-05 00:09:24 +0530

    Update BatteryParametersSender.cs

--- BatteryParametersSender/BatteryParametersSender.cs
+++ BatteryParametersSender/BatteryParametersSender.cs
@@ -6,11 +6,11 @@ namespace BatteryParametersSender
   public class BatteryDataSender
   {
     int[] arr=new int[2];
-      Public string PrintData(arr)
+      public string PrintData(arr)
       {
         for(int i=0;i<arr.Length;i++)
         {
-          return arr.ToString();
+          return arr[i].ToString();
         }
         //Console.WriteLine("Welcome to BMS Streamer");
         //Console.WriteLine("25");
