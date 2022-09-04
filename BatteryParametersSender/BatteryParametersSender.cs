053fe26cc6817769a8c20983a5810502cb152974
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-09-04 21:29:55 +0530

    Update BatteryParametersSender.cs

--- BatteryParametersSender/BatteryParametersSender.cs
+++ BatteryParametersSender/BatteryParametersSender.cs
@@ -1,1 +1,14 @@
+using System;
+using System.Linq;
 
+namespace BatteryParametersSender
+{
+  public class BatteryDataSender
+  {
+      Public void PrintData()
+      {
+        Console.WriteLine("Welcome to BMS Streamer");
+        Console.WriteLine("25");
+      }
+  }  
+}
