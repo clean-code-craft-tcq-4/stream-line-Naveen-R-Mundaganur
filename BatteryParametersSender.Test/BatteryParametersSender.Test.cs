c13f6cae77347e578709e4452f03814fab7106cd
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-09-05 11:04:35 +0530

    Update BatteryParametersSender.Test.cs

--- BatteryParametersSender.Test/BatteryParametersSender.Test.cs
+++ BatteryParametersSender.Test/BatteryParametersSender.Test.cs
@@ -10,9 +10,9 @@ namespace BatteryParametersSender.Test
     [Fact]
     public void TestBatteryParams()
     {
-      if(Assert.True(BatteryDataSender.printData(1)=="1"))
+      if(Assert.True(BatteryDataSender.printData(1)==1))
          Console.WriteLine(1);
-       else if(Assert.True(BatteryDataSender.printData(2)=="2"))
+       else if(Assert.True(BatteryDataSender.printData(2)==2))
           Console.WriteLine(2);
     }
     
