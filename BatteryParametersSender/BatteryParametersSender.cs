9a8b5afcb1a97c50fd0a496af36a5be9ca362a14
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-09-05 11:05:11 +0530

    Update BatteryParametersSender.cs

--- BatteryParametersSender/BatteryParametersSender.cs
+++ BatteryParametersSender/BatteryParametersSender.cs
@@ -6,9 +6,9 @@ namespace BatteryParametersSender
   public class BatteryDataSender
   {
     int[] arr=new int[2];
-      public static string printData(int x)
+      public static int printData(int x)
       {
-        return x.ToString();
+        return x;
         /*for(int i=0;i<arr.Length;i++)
         {
           return arr[i].ToString();
