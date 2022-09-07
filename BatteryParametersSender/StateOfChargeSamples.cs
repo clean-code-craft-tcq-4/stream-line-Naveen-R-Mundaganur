f541cdd15c214a0dc86dcd31648af2d2d17e5328
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-09-07 23:31:59 +0530

    Create StateOfChargeSamples.cs

---
+++ BatteryParametersSender/StateOfChargeSamples.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace BatteryParametersSender
+{
+  public class StateOfCharge
+  {
+    public static List<int> GenerateStateOfCharge(int state_of_charge_readings_length, int soc_minimum, int soc_maximum)
+    {
+            List<int> soc_data = new List<int>();
+            for (int temp_index = 0; temp_index < state_of_charge_readings_length; temp_index++)
+                soc_data.Add(Program.RandomNumber(soc_minimum, soc_maximum));
+            return soc_data;
+    }
+  }
+}
