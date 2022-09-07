d25a2d7ea3124352737129b44b6f966c7f2370e5
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-09-08 03:21:18 +0530

    Update BatteryParametersSender.cs

--- BatteryParametersSender/BatteryParametersSender.cs
+++ BatteryParametersSender/BatteryParametersSender.cs
@@ -6,27 +6,29 @@ namespace BatteryParametersSender
 {
   public class BatteryDataSender
   {    
+      //Class Fields
       private static readonly Random _random = new Random();
       private const int SOC_MIN = 0;
       private const int SOC_MAX = 80;
       private const int TEMP_MIN = -50;
       private const int TEMP_MAX = 200;
-      private const int READING_MAX = 50;                
-                
+      private const int MAX_READING = 50;                
+      
+      //Methods collects the battery system parameters and process for Display
       public static bool GatherBatteryTelemetryDetails()
       {           
             bool isDataPrintable = false;
             List<int> temperature_data = new List<int>();
             List<int> soc_data = new List<int>();
-            temperature_data = GenerateTemperatures(READING_MAX, TEMP_MIN, TEMP_MAX);
-            soc_data = GenerateStateOfCharge(READING_MAX, SOC_MIN, SOC_MAX);
+            temperature_data = GenerateBatteryParameterSamples(MAX_READING, TEMP_MIN, TEMP_MAX);
+            soc_data = GenerateBatteryParameterSamples(MAX_READING, SOC_MIN, SOC_MAX);
             if(IsBatteryParametersEmpty(temperature_data,soc_data))              
             {
               isDataPrintable=DisplayBatteryTelemetryDetails(temperature_data, soc_data);  
             }
             return isDataPrintable;
       }
-      
+      //Check the battery parameters are null
       public static bool IsBatteryParametersEmpty(List<int> temperature_data, List<int> soc_data)
       {
           bool isbatteryDataAvailable=false;
@@ -36,11 +38,11 @@ namespace BatteryParametersSender
           }          
             return isbatteryDataAvailable;
       }
-    
+      //Display the Battery Parameters to Console Output
       public static bool DisplayBatteryTelemetryDetails(List<int> temperature_data, List<int> soc_data)
       {
         bool isPrintable = false;
-            if (temperature_data.Count == READING_MAX && soc_data.Count == READING_MAX)
+            if (temperature_data.Count == MAX_READING && soc_data.Count == MAX_READING)
             {
                 Console.WriteLine($"Temperature Reading, State Of Charge Reading");
                 for (int index = 0; index < READING_MAX; index++)
@@ -57,39 +59,25 @@ namespace BatteryParametersSender
       {
             return _random.Next(minimum_value, maximum_value);           
       }
-      
-      public static List<int> GenerateTemperatures(int temperature_readings_length, int temperature_minimum, int temperature_maximum)
+    
+      //Methods generates the Battery Parameters Samples and process for Display
+      public static List<int> GenerateBatteryParameterSamples(int battery_param_readings_length, int battery_param_minimum, int battery_param_maximum)
       {
-          List<int> temperature_samples = new List<int>();
-            if (temperature_readings_length>0)
+          List<int> battery_parameter_samples = new List<int>();
+            if (battery_param_readings_length>0)
             {
-              for(int temp_index=0;temp_index< temperature_readings_length; temp_index++)
+              for(int temp_index=0;temp_index< battery_param_readings_length; temp_index++)
               {
-                  temperature_samples.Add(GenerateRandomNumber(temperature_minimum, temperature_maximum));
+                  battery_parameter_samples.Add(GenerateRandomNumber(battery_param_minimum, battery_param_maximum));
               }        
-              return temperature_samples;
+              return battery_parameter_samples;
             }
             else
             {
-              temperature_samples=null;
-              return temperature_samples;
+              battery_parameter_samples=null;
+              return battery_parameter_samples;
             }
             
-      }
-      public static List<int> GenerateStateOfCharge(int state_of_charge_readings_length, int soc_minimum, int soc_maximum)
-      {
-          List<int> soc_samples = new List<int>();
-            if (state_of_charge_readings_length>0)
-            {              
-              for (int temp_index = 0; temp_index < state_of_charge_readings_length; temp_index++)
-                  soc_samples.Add(GenerateRandomNumber(soc_minimum, soc_maximum));
-              return soc_samples;
-            }
-            else
-            {  
-               soc_samples=null;
-               return soc_samples;
-            }  
-      }
+      } 
   }  
 }
