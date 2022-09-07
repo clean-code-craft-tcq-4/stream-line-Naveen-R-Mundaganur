672c7c54478f223bee72f419e293cca61ad556f2
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-09-08 01:58:28 +0530

    Update BatteryParametersSender.cs

--- BatteryParametersSender/BatteryParametersSender.cs
+++ BatteryParametersSender/BatteryParametersSender.cs
@@ -8,9 +8,9 @@ namespace BatteryParametersSender
   {    
       private static readonly Random _random = new Random();
       private const int SOC_MIN = 0;
-      private const int SOC_MAX = 100;
+      private const int SOC_MAX = 80;
       private const int TEMP_MIN = -50;
-      private const int TEMP_MAX = 150;
+      private const int TEMP_MAX = 200;
       private const int READING_MAX = 50;                
                 
       public static bool GatherBatteryTelemetryDetails()
@@ -20,13 +20,22 @@ namespace BatteryParametersSender
             List<int> soc_data = new List<int>();
             temperature_data = GenerateTemperatures(READING_MAX, TEMP_MIN, TEMP_MAX);
             soc_data = GenerateStateOfCharge(READING_MAX, SOC_MIN, SOC_MAX);
-            isDataPrintable=DisplayBatteryTelemetryDetails(temperature_data, soc_data);
+            if(temperature_data!=null && soc_data!=null)         
+            {
+              isDataPrintable=DisplayBatteryTelemetryDetails(temperature_data, soc_data);
+            }
+            else
+            {
+              isDataPrintable=false;
+            }
             return isDataPrintable;
       }
       
       public static bool DisplayBatteryTelemetryDetails(List<int> temperature_data, List<int> soc_data)
       {
-            bool isPrintable = false;
+          bool isPrintable = false;
+          if(temperature_data!=null && soc_data!=null)
+          {
             if (temperature_data.Count == READING_MAX && soc_data.Count == READING_MAX)
             {
                 Console.WriteLine($"Temperature Reading, State Of Charge Reading");
@@ -36,28 +45,53 @@ namespace BatteryParametersSender
                 }
                 isPrintable = true;
             }
+          }
+          else
+          {
+            isPrintable=false;
+          }
             return isPrintable;
       }
     
       //This method generates the Random Number for specified Minimum and Maximum range
-      public static int GenerateRandomNumber(int min, int max)
+      public static int GenerateRandomNumber(int minimum_value, int maximum_value)
       {
-            return _random.Next(min, max);
+            if (minimum_value >= int.MinValue && maximum_value <= int.MaxValue)
+                return _random.Next(minimum_value, maximum_value);
+            else
+                return 0;
       }
       
       public static List<int> GenerateTemperatures(int temperature_readings_length, int temperature_minimum, int temperature_maximum)
       {
-            List<int> temperature_samples = new List<int>();
-            for(int temp_index=0;temp_index< temperature_readings_length; temp_index++)
-                temperature_samples.Add(GenerateRandomNumber(temperature_minimum, temperature_maximum));
-            return temperature_samples;
+            if (temperature_readings_length>0)
+            {
+              List<int> temperature_samples = new List<int>();
+              for(int temp_index=0;temp_index< temperature_readings_length; temp_index++)
+              {
+                  temperature_samples.Add(GenerateRandomNumber(temperature_minimum, temperature_maximum));
+              }        
+              return temperature_samples;
+            }
+            else
+            {
+              return temperature_samples=null;
+            }
+            
       }
       public static List<int> GenerateStateOfCharge(int state_of_charge_readings_length, int soc_minimum, int soc_maximum)
       {
-            List<int> soc_samples = new List<int>();
-            for (int temp_index = 0; temp_index < state_of_charge_readings_length; temp_index++)
-                soc_samples.Add(GenerateRandomNumber(soc_minimum, soc_maximum));
-            return soc_samples;
+             if (state_of_charge_readings_length>0)
+            {
+              List<int> soc_samples = new List<int>();
+              for (int temp_index = 0; temp_index < state_of_charge_readings_length; temp_index++)
+                  soc_samples.Add(GenerateRandomNumber(soc_minimum, soc_maximum));
+              return soc_samples;
+             }
+             else
+             {
+                return soc_samples=null;
+             }  
       }
   }  
 }
