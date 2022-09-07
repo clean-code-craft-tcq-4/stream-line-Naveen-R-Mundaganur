1b2607b07d65e059e5573ad0e57618a07fc45113
Naveen-R-Mundaganur <105204623+Naveen-R-Mundaganur@users.noreply.github.com>
2022-09-08 02:31:28 +0530

    Update BatteryParametersSender.Test.cs

--- BatteryParametersSender.Test/BatteryParametersSender.Test.cs
+++ BatteryParametersSender.Test/BatteryParametersSender.Test.cs
@@ -12,16 +12,39 @@ namespace BatteryParametersSender.Test
     public void TestBatteryParams()
     {
       bool result=BatteryDataSender.GatherBatteryTelemetryDetails();
-      Assert.True(result);
-      
-      int result1=BatteryDataSender.GenerateRandomNumber(1,1);
-      Assert.Equal(1,result1);
-      
+      Assert.True(result);                        
+    }
+    
+    [Fact]
+    public void ValidateConsoleOutput()
+    {
       List<int> temp_range=new List<int>(){1,2,3,4,5};
       List<int> soc_range=new List<int>(){2,4,6,8};
-      bool result3=BatteryDataSender.DisplayBatteryTelemetryDetails(temp_range,soc_range);
-      Assert.False(result3);
+      bool result=BatteryDataSender.DisplayBatteryTelemetryDetails(temp_range,soc_range);
+      Assert.False(result);
     }
+    
+    [Fact]
+    public void RandomNumberMethod()
+    {
+      int result=BatteryDataSender.GenerateRandomNumber(1,1);
+      Assert.Equal(1,result);
+    }
+
+    [Fact]
+    public void CheckEmptyData()
+    {
+      List<int> temp_range=new List<int>();
+      List<int> soc_range=new List<int>(){2,4,6,8};
+      //bool result=BatteryDataSender.DisplayBatteryTelemetryDetails(temp_range,soc_range);
+      Assert.False(result); 
+    }
+     [Fact]
+    public void GetBatteryParametersforZeroLength()
+    {
+      
+    }
+      
     
   }
 }
